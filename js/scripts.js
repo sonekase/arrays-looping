3f52f4138c89db0a60ec86f79fce02e4b1932c8c
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-06-08 12:40:54 -0700

    Add css code to stylize application, unable to get animate to work.

--- js/scripts.js
+++ js/scripts.js
@@ -2,31 +2,32 @@
 var beepboopFunction = function(number){
   var result = [];
 
+// set-up: if, else if, else.
   if(number < 0){
     return "Enter higher number."
-  }else if(number > 0 && number%3 === 0){
+    }else if(number > 0 && number%3 === 0){
     return "I'm sorry, Dave. I'm afraid I can't do that.";
-  } else if(number.toString().indexOf('1') > -1){
+    } else if(number.toString().indexOf('1') > -1){
     return "Boop!";
+    } else if(number%10 === 0){
+    return "Beep!";
+    } else {
+    for(var i = 0; i <= number; i += 1){
+      result.push(i);
+      };
+    }
+    return result.toString();
   }
-}
-
-
-
-
-
-
-
 
 
 
 // user interface
 $(document).ready(function(){
   $("#beepboop-form").submit(function(event){
-      event.preventDefault();
-      var userInput = parseInt($('input#message-input').val());
-      var userResult = beepboopFunction(userInput);
-      $("#message-output").text(userResult);
-      $('#input-form').trigger('reset');
+    event.preventDefault();
+    var userInput = parseInt($('input#message-input').val());
+    var userResult = beepboopFunction(userInput);
+    $("#message-output").text(userResult);
+    $('#input-form').trigger('reset');
   });
 });
